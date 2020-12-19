f8bac1f980dfca96afbc2f8fdf622f28a0480e8c
José Armando <striker_skazy@hotmail.com>
2020-12-18 21:09:14 -0300

    mudei nome da classe main

--- src/principal/Application.java
+++ src/principal/Application.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class StrikerMain {
+public class Application {
 
 	public static void main(String[] args) {
 
